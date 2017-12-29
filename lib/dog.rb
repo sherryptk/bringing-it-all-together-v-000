42c3eccdae9ba80e86924174ec8c733649e85d5e
Sherry El Garhy <sherryptk@gmail.com>
2017-12-29 22:33:19 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -50,7 +50,6 @@ class Dog
  def self.find_by_id(id)
     sql = "SELECT * FROM dogs WHERE id = ?"
     result = DB[:conn].execute(sql, id)[0]
-    binding.pry
     Dog.new(result[0], result[1], result[2])
   end
 
